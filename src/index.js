d46d5f96bd45c7c22ab306a44df94bd7c3f6b113
AlekEngineer <calessin@gmail.com>
2018-09-17 23:22:24 +0300

    100 passing

--- src/index.js
+++ src/index.js
@@ -4,4 +4,17 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
-};
+  var amt=0;
+    for (var i1=0; i1<preferences.length; i1++) {
+      for(var i2=0; i2<preferences.length; i2++) {
+        for(var i3=0; i3<preferences.length; i3++) {
+          if (i1!=i2 && i2!=i3 && i3!=i1){ 
+          if ((i1+1==preferences[i2])&&(i2+1==preferences[i3])&&(i3+1==preferences[i1])) {
+         amt=amt+1;
+          }  
+        }
+      }  
+    }
+  }
+  return (amt/3);
+}
